e89f411610d6f2a5ee51f81090fbb1d101d29f73
Alex Tran <atran116@gmail.com>
2016-06-07 23:56:06 +0000

    Merge changes

---
+++ save_record.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+X=$(date -d "today" +"%Y%m%d%H%M")
+echo $X
+arecord -D plughw:1,0 -f dat -vv /home/pi/Desktop/Final_Project/audioboard/ARecord/recordings/${X}_Input.wav
+
+echo "Uploading to DropBox\n"
+./dropbox_uploader.sh upload /home/pi/Desktop/Final_Project/audioboard/ARecord/recordings/${X}_Input.wav ${X}_Input.wav
